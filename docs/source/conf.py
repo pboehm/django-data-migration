c27afb406df67c0112ab8f5de5fea4f070fbbf8f
Philipp Böhm <philipp-boehm@live.de>
2014-01-26 16:56:57 +0100

    Added rtd-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,9 @@ from os.path import dirname
 
 import sys
 import os
+import sphinx_rtd_theme
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 p = os.path.abspath(dirname(dirname(dirname(__file__))))
 sys.path.append(p)
@@ -109,7 +112,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
