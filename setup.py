36acf2da1f73cc8fb70fed2c99973e070992ca97
Philipp Böhm <philipp-boehm@live.de>
2014-01-28 12:14:39 +0100

    Changed README to RST

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open('data_migration/version.py') as f:
     exec(f.read())
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='django-data-migration',
